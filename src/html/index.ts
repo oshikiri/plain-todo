e04f35b4a13cc06ce5502075ccbb69244fd65965
Takamasa Oshikiri <mail@oshikiri.org>
2022-01-15 00:29:52 +0900

    Implement watch mode for html subcommand (#17)
    * Refactor toHtml
    
    * Implement watch mode for html subcommand
    
    * Set html title
    
    * Implement watch mode using browser-sync

--- src/html/index.ts
+++ src/html/index.ts
@@ -3,6 +3,8 @@ import * as fs from "fs";
 import dayjs = require("dayjs");
 import * as marked from "marked";
 
+const browserSync = require("browser-sync");
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   pedantic: false,
@@ -13,60 +15,95 @@ marked.setOptions({
 });
 
 import * as io from "../io";
-import { Arguments } from "../types";
+import { Arguments, Task } from "../types";
 import * as utils from "../utils";
+import path = require("path");
 
 export function mainHtml(argv: Arguments) {
-  const yamlPath = argv.file;
-  if (!io.fileExists(yamlPath)) {
-    console.log(`Specified file does not exist: ${yamlPath}`);
-    return;
+  if (argv.watch) {
+    if (argv.out) {
+      console.log(`Overwrite argv.out because of watch mode`);
+    }
+    const dir = "/tmp/plain-todo";
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+    argv.out = `${dir}/index.html`;
   }
-  const yml = io.loadYaml(yamlPath);
 
-  const maxLengthTaskName = 100;
-  let tasks = utils.extractSubtree(yml.tasks, argv.subtree);
-  tasks = utils.fillParentsInformations(
-    tasks,
-    "",
-    [undefined],
-    [undefined],
-    maxLengthTaskName,
-    yamlPath
-  );
+  let prevHtml = "";
+  function loop() {
+    const yamlPath = argv.file;
+    if (!io.fileExists(yamlPath)) {
+      console.log(`Specified file does not exist: ${yamlPath}`);
+      return;
+    }
+    const yml = io.loadYaml(yamlPath);
 
-  tasks = utils
-    .flattenTasks(tasks)
-    .filter((t) => t.children.length == 0 && t.status != "todo")
-    .sort((t1, t2) => dayjs(t1.end).diff(dayjs(t2.end)));
+    const maxLengthTaskName = 100;
+    let tasks = utils.extractSubtree(yml.tasks, argv.subtree);
+    tasks = utils.fillParentsInformations(
+      tasks,
+      "",
+      [undefined],
+      [undefined],
+      maxLengthTaskName,
+      yamlPath
+    );
 
+    tasks = utils
+      .flattenTasks(tasks)
+      .filter((t) => t.children.length == 0 && t.status != "todo")
+      .sort((t1, t2) => dayjs(t1.end).diff(dayjs(t2.end)));
+
+    const nextHtml = toHtml(yml.title, tasks);
+    if (prevHtml != nextHtml) {
+      fs.writeFileSync(argv.out, nextHtml);
+      browserSync.reload(argv.out);
+      prevHtml = nextHtml;
+    }
+  }
+  loop();
+  if (argv.watch) {
+    browserSync.init({
+      server: path.dirname(argv.out),
+    });
+    setInterval(loop, 1000);
+  }
+}
+
+function toHtml(title: string, tasks: Task[]) {
   // TODO: use DOM library
-  console.log(`
+  const htmlTags = [];
+  htmlTags.push(`
     <head>
       <meta charset="UTF-8">
+      <title>${title}</title>
     </head>
     <body>
   `);
-  console.log(`
+  htmlTags.push(`
     <style>
         ${fs.readFileSync(__dirname + "/style.css", "utf8")}
     </style>
   `);
-  console.log(`<div class="tasks">`);
+  htmlTags.push(`<div class="tasks">`);
   for (const task of tasks) {
-    console.log(`<div class="task">`);
-    console.log(`<div class="task-name">${task.absolutePath}</div>`);
+    htmlTags.push(`<div class="task">`);
+    htmlTags.push(`<div class="task-name">${task.absolutePath}</div>`);
     const start = task.start ? dayjs(task.start).format("YYYY-MM-DD") : "";
     const end = task.end ? dayjs(task.end).format("YYYY-MM-DD") : "";
-    console.log(`<div class="task-dates">
+    htmlTags.push(`<div class="task-dates">
       <span class="task-date">${start}</span> ― <span class="task-date">${end}</span>
       <span class="task-status">${task.status}</span>
     </div>`);
     if (task.memo) {
-      console.log(marked(task.memo));
+      htmlTags.push(marked(task.memo));
     }
-    console.log("</div>");
+    htmlTags.push("</div>");
   }
-  console.log("</div>");
-  console.log("</body>");
+  htmlTags.push("</div>");
+  htmlTags.push("</body>");
+
+  return htmlTags.join("\n");
 }
